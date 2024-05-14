de2bb1ce5317115488a24adc509c9ceca603f054
Subhajitorrin <orrin2op@gmail.com>
2024-05-14 22:21:21 +0530

    add py file

---
+++ automateMail.py
@@ -0,0 +1,25 @@
+b) A key that represents a link between columns in two different tables
+c) ALTER TABLE
+b) They enforce referential integrity between child and parent tables
+c) To restrict the range of acceptable values for a column
+c) The foreign key column and the referenced key column must have similar data types
+b) ADD COLUMN
+c) They enforce data integrity and prevent orphan records
+c) Modifies the structure of an existing table
+c) UNIQUE constraint
+c) Both during table creation and through ALTER TABLE command
+c) To enforce data integrity
+c) Referenced table
+b) Ensuring that foreign keys reference existing primary keys
+b) Reject the insertion with an error message
+c) The primary key column of the referenced table
+d) NOT NULL constraint
+c) To restrict the range of acceptable values for a column
+a) ADD CONSTRAINT
+c) Both at the column level and table level
+b) They simplify data management
+
+
+
+
+
